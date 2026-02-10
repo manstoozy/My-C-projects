27f5baba8ea544d0a32c1d5d914c37eaa983e5e4
Sibusiso Cebekhulu <manstoozy285@gmail.com>
2026-02-10 12:24:44 +0200

    Add files via upload

---
+++ Calculate the area of the rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Assigning the variables
+	int length = 6;
+	int width = 12;
+	
+	// Calculating the area of the rectangle
+	int area = length * width;
+	
+	// Print the value of area using cout
+	cout << "Lenght: " << length << "\n";
+	cout << "Width: " << width << "\n";
+	cout << "Area of the rectangle is: " << area << "\n";
+	return 0;
+}
---
+++ Program that stores data about a college student.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {//student data
+	int studentID = 15;
+	int studentAGE = 20;
+	double studentFee = 75.25;
+	char studentGrade = 'A';
+	
+	// Print variables using cout
+	cout << "Student ID: " << studentID << "\n";
+	cout << "Student Age: " << studentAGE << "\n";
+	cout << "Student Fee: " << studentFee << "\n";
+	cout << "Student Grade: " << studentGrade << "\n";
+	return 0;
+	
+}
